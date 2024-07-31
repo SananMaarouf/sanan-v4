aa8ceef1410b94e5dea84c40b59388d73d020c10
Sanan Maarouf <sanan.adnan97@gmail.com>
2024-07-31 02:20:31 +0200

    the nav is now in my humble opinion *chefs kiss*. it's nothing special i am just proud of just making this from scratch instead of just copying a template online and changing that to fit my needs.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,8 @@
+import Link from 'next/link';
 import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faTimes, faHome } from '@fortawesome/free-solid-svg-icons';
-import { Repeat } from 'lucide-react';
-import { exit } from 'process';
 
 export default function Navbar() {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -37,12 +35,12 @@ export default function Navbar() {
     };
 
     return (
-        <nav className='border-red-500 border-4 px-5'>
+        <nav className='px-5 bg-black'>
             <motion.div
                 initial={{ y: -100 }}
                 animate={{ y: 0 }}
                 transition={{ type: "spring", stiffness: 100, damping: 17 }} // Adjust transition properties
-                className='flex flex-row items-center place-content-between py-2'
+                className='hidden md:flex flex-row items-center place-content-between py-2 mx-auto w-3/4'
             >
                 {/* home btn */}
                 <section className=''>
@@ -54,7 +52,79 @@ export default function Navbar() {
                             }}
                             whileTap={{ scale: 1.2, transition: { duration: 0.2 } }}
                         >
-                        <FontAwesomeIcon icon={faHome} size='2xl' className='' />
+                            <h2 className='font-bold text-3xl'>SM</h2>
+                        </motion.button>
+                    </Link>
+                </section>
+                {/* nav menu */}
+                <section className='z-50 flex flex-row w-3/4 place-content-evenly'>
+                    <Link href={"/projects"}>
+                        <motion.button
+                            className='hover:underline underline-offset-2'
+                            whileHover={{
+                                scale: 1.2,
+                                transition: { duration: 0.2 },
+                            }}
+                            whileTap={{ scale: 1.2, transition: { duration: 0.2 } }}
+                        >
+                            Projects
+                        </motion.button>
+                    </Link>
+                    <Link href={"#contact"}>
+                        <motion.button
+                            className='hover:underline underline-offset-2'
+                            whileHover={{
+                                scale: 1.2,
+                                transition: { duration: 0.2 },
+                            }}
+                            whileTap={{ scale: 1.2, transition: { duration: 0.2 } }}
+                        >
+                            Resume
+                        </motion.button>
+                    </Link>
+                    <Link href={"#contact"}>
+                        <motion.button
+                            className='hover:underline underline-offset-2'
+                            whileHover={{
+                                scale: 1.2,
+                                transition: { duration: 0.2 },
+                            }}
+                            whileTap={{ scale: 1.2, transition: { duration: 0.2 } }}
+                        >
+                            Github
+                        </motion.button>
+                    </Link>
+                    <Link href={"#contact"}>
+                        <motion.button
+                            className='hover:underline underline-offset-2'
+                            whileHover={{
+                                scale: 1.2,
+                                transition: { duration: 0.2 },
+                            }}
+                            whileTap={{ scale: 1.2, transition: { duration: 0.2 } }}
+                        >
+                            Contact
+                        </motion.button>
+                    </Link>
+                </section>
+            </motion.div>
+            <motion.div
+                initial={{ y: -100 }}
+                animate={{ y: 0 }}
+                transition={{ type: "spring", stiffness: 100, damping: 17 }} // Adjust transition properties
+                className='flex flex-row items-center place-content-between py-2 md:hidden'
+            >
+                {/* home btn */}
+                <section className=''>
+                    <Link href={"/"}>
+                        <motion.button
+                            whileHover={{
+                                scale: 1.2,
+                                transition: { duration: 0.2 },
+                            }}
+                            whileTap={{ scale: 1.2, transition: { duration: 0.2 } }}
+                        >
+                        <FontAwesomeIcon icon={faHome} size='2x' className='' />
                         </motion.button>
                     </Link>
                 </section>
@@ -65,7 +135,7 @@ export default function Navbar() {
                         <AnimatePresence initial={false} mode='wait'>
                             <motion.button 
                                 key={"close"}
-                                exit={{ opacity: 0, transform: 'rotate(180deg)' }}
+                                exit={{ opacity: 0, }}
                                 transition={{ duration: 0.3 }}
                                 whileHover={{
                                     scale: 1.2,
@@ -75,7 +145,7 @@ export default function Navbar() {
                                 }
                                 onClick={toggleMenu} 
                                 className='btn w-10 h-10 z-50'>
-                                <FontAwesomeIcon icon={faTimes} size='2xl' />
+                                <FontAwesomeIcon icon={faTimes} size='2x' />
                             </motion.button>
                         </AnimatePresence>
                         :
@@ -83,7 +153,7 @@ export default function Navbar() {
                         <AnimatePresence initial={false} mode='wait'>
                             <motion.button
                                 key={"open"} 
-                                exit={{ opacity: 0, transform: 'rotate(180deg)' }}
+                                exit={{ opacity: 0, }}
                                 transition={{ duration: 0.3 }}
 
                                 whileHover={{
@@ -94,11 +164,9 @@ export default function Navbar() {
                                     scale: 1.2,
                                     transition: { duration: 0.1 },
                                 }}
-                                onClick={()=> {
-                                    toggleMenu();
-                                }} 
+                                onClick={toggleMenu} 
                                 className='btn w-10 h-10 z-50'>
-                                <FontAwesomeIcon icon={faBars} size='2xl' />
+                                <FontAwesomeIcon icon={faBars} size='2x' />
                             </motion.button>
                         </AnimatePresence>
                     }
@@ -107,28 +175,41 @@ export default function Navbar() {
             </motion.div>
             <AnimatePresence>
                 {menuOpen && (
-                    <motion.section
-                        key={"menu"}
-                        initial='hidden'
-                        animate='visible'
-                        exit='hidden'
-                        variants={menuVariants}
-                        transition={{ type: 'inertia', duration: 0.4 }}
-                        className='rounded-l-3xl fixed z-30 top-0 right-0 w-3/4 h-full bg-black border border-white'
-                    >
-                        <div className='flex flex-col space-y-10 p-10 mt-10 text-3xl'>
-                            <motion.div variants={itemVariants}>
-                                <Link onClick={toggleMenu} href={"/projects"} className='hover:underline underline-offset-2'>
-                                    Projects
-                                </Link>
-                            </motion.div>
-                            <motion.div variants={itemVariants}>
-                                <Link onClick={toggleMenu} href={"#contact"} className='hover:underline underline-offset-2'>
-                                    Contact
-                                </Link>
-                            </motion.div>
-                        </div>
-                    </motion.section>
+                    <section>
+                        <div onClick={toggleMenu} className='fixed z-30 top-0 left-0 w-1/4 h-full'></div>
+                        <motion.section
+                            key={"menu"}
+                            initial='hidden'
+                            animate='visible'
+                            exit={{ display:"hidden", opacity: 0, x: 100 }}
+                            variants={menuVariants}
+                            transition={{ type: 'tween', duration: 0.2 }}
+                            className='rounded-l-3xl fixed z-30 top-0 right-0 w-3/4 h-full bg-black border border-white'
+                        >
+                            <section className='flex flex-col space-y-10 p-10 mt-10 text-3xl'>
+                                <motion.div variants={itemVariants}>
+                                    <Link onClick={toggleMenu} href={"/projects"} className='hover:underline underline-offset-2'>
+                                        Projects
+                                    </Link>
+                                </motion.div>
+                                <motion.div variants={itemVariants}>
+                                    <Link onClick={toggleMenu} href={"#contact"} className='hover:underline underline-offset-2'>
+                                        Resume
+                                    </Link>
+                                </motion.div>
+                                <motion.div variants={itemVariants}>
+                                    <Link onClick={toggleMenu} href={"#contact"} className='hover:underline underline-offset-2'>
+                                        Github
+                                    </Link>
+                                </motion.div>
+                                <motion.div variants={itemVariants}>
+                                    <Link onClick={toggleMenu} href={"#contact"} className='hover:underline underline-offset-2'>
+                                        Contact
+                                    </Link>
+                                </motion.div>
+                            </section>
+                        </motion.section>
+                    </section>
                 )}
             </AnimatePresence>
         </nav>
