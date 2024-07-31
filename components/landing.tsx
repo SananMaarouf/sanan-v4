322510b96e71de9ddc083b8a71b55679139cc310
Sanan Maarouf <sanan.adnan97@gmail.com>
2024-07-31 15:43:51 +0200

    the landing is nice now i guess, starting on projects slider

--- components/landing.tsx
+++ components/landing.tsx
@@ -1,30 +1,66 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
-
+import portrait from '../public/sanan.png';
 export default function Landing() {
     return (
-        <motion.section 
-            initial={{y: 100, opacity: 0}} 
-            animate={{opacity: 100, y: 0}} 
-            transition={{duration: 0.4}}
-            className="flex flex-row place-items-center border-4 border-blue-700 max-w-screen-lg mx-auto">
-            <section className="flex flex-col border-4 border-green-700 mx-auto text-center">
-                <h1 className="text-4xl font-semibold underline">Sanan Maarouf</h1>
-                <Image 
-                    src="/sanan.png" alt="Portrait of Sanan" 
-                    width={200} height={200} className='mx-auto'
+        /* parent section */
+        <motion.div
+            initial={{ y: 100, opacity: 0 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.4 }}
+            className="
+            mx-auto
+            mt-4 
+            flex 
+            flex-col 
+            overflow-hidden 
+            rounded-2xl 
+            border 
+            border-white
+            bg-transparent 
+            bg-opacity-5
+            bg-clip-padding 
+            backdrop-blur-sm 
+            backdrop-filter 
+            md:my-5
+            md:w-1/2 
+            md:flex-row
+            "
+        >
+            <div className="xl:pl-12 relative my-auto flex flex-1 basis-1/2 flex-col px-4 py-6 lg:py-12 ">
+                <div className='flex flex-row mx-auto md:mx-0 md:flex-col'>
+                <h1 className="text-3xl md:text-4xl lg:text-5xl">
+                    Hi
+                    <motion.div
+                        className="w-fit inline-block" animate={{ rotate: [0, 10, -10, 0] }}
+                        transition={{ duration: 1, repeat: Infinity, repeatDelay: 2 }}>
+                        👋🏽
+                    </motion.div>
+                </h1>
+                <h2 className="text-3xl ml-2 md:ml-0 md:mt-4 md:text-4xl lg:text-5xl">
+                    I'm Sanan
+                </h2>
+                            </div>
+                <div className="flex grow flex-col justify-between">
+                    <div className='flex flex-row md:flex-col w-full place-content-between'>
+                        <p className="mt-2 text-sm md:mt-4 md:text-lg lg:text-xl">
+                            Full Stack Developer
+                        </p>
+                        <p className="mt-2 text-sm md:mt-4 md:text-lg lg:text-xl">
+                            📍 Oslo, Norway
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="flex justify-center bg-transparent md:max-w-lg lg:max-w-xl">
+                <Image
+                    src={portrait}
+                    width={300}
+                    height={400}
+                    alt="Portrait of Sanan"
                 />
-                <section className='flex flex-row mx-auto place-content-evenly md:place-content-around w-full'>
-                    <p className="text-md md:text-lg font-semibold">Fullstack Developer</p>
-                    <p className="text-md md:text-lg font-semibold">Based in Oslo, Norway. </p>                    
-                </section>
-                <p> 
-                I've been interested in computers for as long as I can remember. 
-                I really enjoy learning new technologies 
-                and use them to create practical solutions.
-                </p>
-            </section>
-      </motion.section>
+            </div>
+        </motion.div>
     );
 }
---
+++ components/projects.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+
+export default function Projects() {
+    return (
+        <motion.div>
+            
+        </motion.div>
+    );
+}
