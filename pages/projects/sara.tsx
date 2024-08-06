99300c87ff171fc33afd6a526e96ea81a3bc861d
Sanan Maarouf <sanan.adnan97@gmail.com>
2024-08-07 01:36:08 +0200

    sara translated

--- pages/projects/sara.tsx
+++ pages/projects/sara.tsx
@@ -51,10 +51,10 @@ export default function Sara() {
                         Sarazamecznik.com
                     </h1>
                     <p className="mt-2 text-md md:mt-4 md:text-lg lg:text-xl">
-                        Portfolio page of Oslo's aspiring artist, Sara Zamecznik
+                        {t("sara.description")}
                     </p>
                     <p className="mt-2 text-sm md:mt-4 md:text-lg lg:text-xl">
-                        February 27th, 2024
+                        {t("sara.date")}
                     </p>
                 </div>
 
@@ -75,82 +75,55 @@ export default function Sara() {
             >
                 <section className="mx-auto my-5 md:my-10 md:w-3/5 lg:w-1/2">
                     <p>
-                        This was an exiting project to do.
+                        {t("sara.intro")}
                     </p>
                 </section>
                 <section className="mx-auto my-5 md:my-10 md:w-3/5 lg:w-1/2">
                     <p>
-                        My client, Sara Zamecznik, is an art student at KHIO in Oslo.
-                        She is a skilled artist who creates art in various mediums, specializing in ceramics.
+                        {t("sara.intro2")}
                     </p>
                 </section>
                 <section className="mx-auto my-5 md:my-10 md:w-3/5 lg:w-1/2">
                     <p>
-                        Upon learning that she would soon graduate and had been showcasing her art exclusively on Instagram,
-                        I recognized the need for a more professional platform—an exclusive website.
+                        {t("sara.intro3")}
                     </p>
                 </section>
                 <section className="mx-auto my-5 md:my-10 md:w-3/5 lg:w-1/2">
                     <p>
-                        I reached out to Sara and presented an offer she couldn't refuse.
-                        I had the freedom to use any technologies I preferred,
-                        with her only requirement being a design that is minimal, mobile-friendly,
-                        and cost-effective to host.
+                        {t("sara.intro4")}
                     </p>
                 </section>
                 <section className="mx-auto my-5 md:my-10 md:w-3/5 lg:w-1/2">
                     <p>
-                        With these criteria in mind, I set out to find a suitable tech stack.
-                        I instantly thought of using a CMS such as Wordpress or Squarespace for their ease of use and are easy to maintain.
-                        While CMS-platforms like WordPress or Squarespace are popular for their user friendlyness,
-                        they come at a literal cost.
-                        Luckily there are more cost effective alternatives.
-                        Given the nature of the site, all off the content could be static and
-                        only need to be updated when she makes a new post.
-                        Here is where static site generators(SSG) and headless-CMS systems come in.
-                        By utilizing SSGs, such as Next.js, to fetch content from the headless CMS, in this case,
-                        Contentful, I could generate a static webpage.
-                        This approach is ideal for achieving a custom UI design and allows for easy hosting on Vercel,
-                        free of charge. It also enables non-coders to create content through a user-friendly CMS interface,
-                        triggering automatic webpage rebuilding via webhooks.
+                        {t("sara.criteria")}
                     </p>
                 </section>
                 <section className="mx-auto my-5 md:my-10 md:w-3/5 lg:w-1/2">
                     <p>
-                        This choice comes with a drawback—changing the UI in the future would either require
-                        coding knowledge or hiring my services again.
-                        After discussing the pros and cons, Sara and I agreed on the headless CMS approach.
+                        {t("sara.choice")}
                     </p>
                 </section>
                 <section className="mx-auto my-5 md:my-10 md:w-3/5 lg:w-1/2">
                     <p>
-                        After doing some research the tech stack ended up being:
+                        {t("sara.research")}
                     </p>
                     <ul>
-                        <li>NextJS as the JS framework</li>
-                        <li>TailwindCSS for styling</li>
-                        <li>Contentful as the headless-CMS system</li>
-                        <li>GraphQL for querying</li>
-                        <li>Formspree for messaging</li>
-                        <li>Vercel for hosting</li>
+                        <li>NextJS {t("sara.techStack.nextjs")}</li>
+                        <li>TailwindCSS {t("sara.techStack.tailwind")} </li>
+                        <li>Contentful {t("sara.techStack.contentful")}</li>
+                        <li>GraphQL {t("sara.techStack.graphql")}</li>
+                        <li>Formspree {t("sara.techStack.formspree")}</li>
+                        <li>Vercel {t("sara.techStack.vercel")}</li>
                     </ul>
                 </section>
                 <section className="mx-auto my-5 md:my-10 md:w-3/5 lg:w-1/2">
                     <p>
-                        Developing with Next.js was enjoyable, thanks to its built-in tools like image optimization and routing.
-                        Hosting on Vercel, which is tailored for Next.js, was seamless.
-                        Tailwind CSS facilitated rapid UI development, making responsive components easy to create.
+                        {t("sara.nextJsExperience")}
                     </p>
                 </section>
                 <section className="mx-auto my-5 md:my-10 md:w-3/5 lg:w-1/2">
                     <p>
-                        Contentful proved to be user-friendly, allowing for straightforward content model creation, post generation, and asset uploads.
-                    </p>
-                </section>
-                <section className="mx-auto my-5 md:my-10 md:w-3/5 lg:w-1/2">
-                    <p>
-                        The website serves as more than a digital gallery; it's a narrative space where each piece tells a story.
-                        Visitors are invited to explore the inspirations and journeys behind Sara's creations.
+                        {t("sara.contentfulExperience")}
                     </p>
                 </section>
 
